f3a1a4f496186a48fdb1df067ecd1515bdbc8cd7
Karen Palacios <karenmpalacios@gmail.com>
2021-11-29 12:38:59 -0800

    Add password recovery view

--- src/store/auth.js
+++ src/store/auth.js
@@ -1,24 +1,7 @@
 import Request from './request'
-import Csrf from './csrf'
 
 export default {
   async register (form) {
-    await Csrf.getCookie()
-
     return Request.post('/register', form)
-  },
-
-  async login (form) {
-    return Request.post('/login', form)
-  },
-
-  async logout () {
-    await Csrf.getCookie()
-
-    return Request.post('/logout')
-  },
-
-  auth () {
-    return Request.get('/user')
   }
 }
---
+++ src/views/Password.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="home col-5 mx-auto py-5 mt-2">
+    <div class="row justify-content-center px-3 mb-3">
+        <img id="logo" class="img-thumbnail" src="https://media.glassdoor.com/sql/922389/tambourine-squarelogo-1495210428848.png">
+    </div>
+    <h1 class="text-center">Reset Password</h1>
+    <div class="card">
+      <div class="card-body">
+        <div class="form-group">
+          <label for="email">Email address:</label>
+          <input
+            type="email"
+            v-model="email"
+            class="form-control"
+            id="email"
+          />
+          <span class="text-danger" v-if="errors.email">
+            {{ errors.email[0] }}
+          </span>
+        </div>
+        <div class="row justify-content-center my-3 px-3">
+        <button @click.prevent="sendPasswordReset" class="btn btn-primary btn-block">
+          Send Password Reset Link
+        </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      email: '',
+      password: '',
+      errors: []
+    }
+  },
+  methods: {
+    sendPasswordReset () {
+      this.$store
+        .dispatch('sendPasswordReset', {
+          email: this.email
+        })
+        .then(() => {
+          this.$router.push({ name: 'Dashboard' })
+        })
+        .catch(error => {
+          if (error.response.status === 422) {
+            this.errors = error.response.data.errors
+          }
+        })
+    }
+  }
+}
+</script>
